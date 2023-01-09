e95c4b1ca9324188b32ff099665ece3323e6564e
Minh Phuong <phuong.tm.nguyen@c2c-techhub.io>
2023-01-09 15:11:06 +0700

    update

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -11,58 +11,6 @@ export const noto = Noto_Sans_JP({
   fallback: ['sans-serif'],
 });
 
-declare module '@mui/material/styles' {
-  interface Palette {
-    white: string;
-    button: string;
-    backgroundColor: string;
-    tertiary: string;
-    heading: string;
-    grown: string;
-  }
-
-  interface PaletteOptions {
-    white?: string;
-    grown?: string;
-    button?: string;
-    backgroundColor: string;
-    heading: string;
-  }
-  interface TypographyVariants {
-    title: React.CSSProperties;
-  }
-
-  // allow configuration using `createTheme`
-  interface TypographyVariantsOptions {
-    title?: React.CSSProperties;
-  }
-}
-
-declare module '@mui/material/InputBase' {
-  interface InputBasePropsSizeOverrides {
-    large: true;
-  }
-}
-
-declare module '@mui/material/styles' {
-  interface BreakpointOverrides {
-    tablet: true;
-    mobile: true;
-  }
-}
-
-declare module '@mui/material/Typography' {
-  interface TypographyPropsVariantOverrides {
-    title: true;
-  }
-}
-
-declare module '@mui/material/Button' {
-  interface ButtonPropsSizeOverrides {
-    xs: true;
-  }
-}
-
 // Create a theme instance.
 const theme = createTheme({
   ...defaultTheme,
