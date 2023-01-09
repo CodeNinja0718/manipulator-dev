5e21997ba8460c6dcb2d1128fddee59cabaf0c3e
vinhnguyenc2c <vinh.nguyen@c2c-techhub.io>
2023-01-09 15:52:51 +0700

    feat/ui_side_menu: update gradient hoc

--- src/hoc/GrandientComponent/index.tsx
+++ src/hoc/GrandientComponent/index.tsx
@@ -4,5 +4,5 @@ import styles from './styles';
 
 export function gradientComponent(element: React.ReactNode, gradient?: string) {
   const gradientColor = gradient ? styles[gradient] : {};
-  return <Box sx={gradientColor}>{element}</Box>;
+  return <Box sx={gradientColor || { background: gradient }}>{element}</Box>;
 }
