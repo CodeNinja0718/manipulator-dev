8d89ff2f3b3d5cb21c3ec009863c90780ed3189c
vinhnguyenc2c <vinh.nguyen@c2c-techhub.io>
2023-01-30 10:11:23 +0700

    chore: Improve common modal on mobile

--- src/components/CommonModal/index.tsx
+++ src/components/CommonModal/index.tsx
@@ -5,19 +5,7 @@ import SvgIcon from '@mui/material/SvgIcon';
 import Typography from '@mui/material/Typography';
 import React from 'react';
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 900,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: '30px 50px 54px',
-  '&:focus-visible': {
-    outline: 'none',
-  },
-};
+import styles from './styles';
 
 interface CommonModalProps {
   title?: string;
@@ -30,6 +18,40 @@ interface CommonModalProps {
   children?: JSX.Element;
 }
 
+const renderCloseButton = (
+  isHideCloseButton?: boolean,
+  iconClose?: React.ElementType,
+  onClose?: () => void,
+) => {
+  if (isHideCloseButton) return false;
+
+  return iconClose ? (
+    <Box width={24} height={24} position="absolute" top={20} right={20}>
+      <SvgIcon
+        component={iconClose}
+        viewBox="0 0 33 33"
+        onClick={onClose}
+        sx={{
+          color: '#949494',
+          cursor: 'pointer',
+        }}
+      />
+    </Box>
+  ) : (
+    <Box width={45} height={45} position="absolute" top={10} right={10}>
+      <SvgIcon
+        component={CloseIcon}
+        onClick={onClose}
+        sx={{
+          color: '#949494',
+          cursor: 'pointer',
+          fontSize: 45,
+        }}
+      />
+    </Box>
+  );
+};
+
 const CommonModal = ({
   title,
   titleElement,
@@ -40,36 +62,6 @@ const CommonModal = ({
   onClose,
   children,
 }: CommonModalProps) => {
-  const renderCloseButton = () => {
-    if (isHideCloseButton) return false;
-
-    return iconClose ? (
-      <Box width={24} height={24} position="absolute" top={20} right={20}>
-        <SvgIcon
-          component={iconClose}
-          viewBox="0 0 33 33"
-          onClick={onClose}
-          sx={{
-            color: '#949494',
-            cursor: 'pointer',
-          }}
-        />
-      </Box>
-    ) : (
-      <Box width={45} height={45} position="absolute" top={10} right={10}>
-        <SvgIcon
-          component={CloseIcon}
-          onClick={onClose}
-          sx={{
-            color: '#949494',
-            cursor: 'pointer',
-            fontSize: 45,
-          }}
-        />
-      </Box>
-    );
-  };
-
   return (
     <>
       <Modal
@@ -78,24 +70,20 @@ const CommonModal = ({
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Box
-            display="flex"
-            justifyContent="center"
-            sx={{
-              width: '100%',
-              margin: 'auto',
-              mb: 36,
-            }}
-          >
+        <Box sx={styles.modalContainer}>
+          <Box display="flex" justifyContent="center" sx={styles.titleModal}>
             {titleElement || <Typography variant="title">{title}</Typography>}
           </Box>
           {/* Close Button */}
-          {renderCloseButton()}
+          {renderCloseButton(isHideCloseButton, iconClose, onClose)}
           {/* Content of Modal */}
-          {children}
+          <Box sx={styles.bodyModal}>{children}</Box>
           {/* Button */}
-          {buttonElement && <Box mt={40}>{buttonElement}</Box>}
+          {buttonElement && (
+            <Box maxWidth={522} m="auto" mt={40}>
+              {buttonElement}
+            </Box>
+          )}
         </Box>
       </Modal>
     </>
---
+++ src/components/CommonModal/styles.ts
@@ -0,0 +1,29 @@
+import type { SxProps, Theme } from '@mui/material/styles';
+
+const styles = {
+  modalContainer: {
+    position: 'absolute' as 'absolute',
+    top: { xs: 0, md: '50%' },
+    left: { xs: 0, md: '50%' },
+    width: '100%',
+    maxWidth: 900,
+    height: { xs: '100vh', md: 'initial' },
+    transform: { xs: 'initial', md: 'translate(-50%, -50%)' },
+    background: 'white',
+    boxShadow: 24,
+    p: { xs: 23, md: '30px 50px 54px' },
+    '&:focus-visible': {
+      outline: 'none',
+    },
+  },
+  titleModal: {
+    width: '100%',
+    margin: 'auto',
+    mb: 36,
+  },
+  bodyModal: {
+    mt: { xs: 40, md: 36 },
+  },
+} as Record<string, SxProps<Theme>>;
+
+export default styles;
